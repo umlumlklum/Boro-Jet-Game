995933acc2f3aebda453cad68f084958806bcef3
NReedel <125296416+NReedel@users.noreply.github.com>
2024-02-07 14:47:02 -0500

    Update main.js
    all instances of Column have been replaced by unique instance identifiers.

--- Jet-Racer/main.js
+++ Jet-Racer/main.js
@@ -1,7 +1,8 @@
 function startGame(){
-    Player = new component(30, 30, "red", playerWidth, playerHeight);
-    Score = new component("30px", "Consolas", "black", 280, 40, "text");
-    Background = new component(canvasWidth, canvasHeight, "city.jpeg", 0, 0, "background");
-    Obstacle.push(new component(10, 200, "green", 300, 120));
+    Player = new player(30, 30, playerWidth, playerHeight, "red"); // player class
+    Score = new text( 280, 40, "30px", "Consolas", "text","black"); // text class
+    Background = new background(0, 0,canvasWidth, canvasHeight,"city.jpeg"); // background class
+    Column.push(new column(10, 200, 300, 120, "green"));
     gameArea.start();
+    
 }
