da33ac8351069e7bcefbe6dab39661e95d8ac229
NReedel <125296416+NReedel@users.noreply.github.com>
2024-02-14 16:03:43 -0500

    Update main.js
    -removed score class 
    -added Statbox and Stats
    -removed instanc of obstacles

--- Jet-Racer/main.js
+++ Jet-Racer/main.js
@@ -1,7 +1,8 @@
 function startGame(){
     player = new Player(30, 30, playerWidth, playerHeight, 0, 0, 3, 1, "red"); // player class
-    score = new Text(280, 40, "30px", "Consolas", "black"); // text class
+    // score = new Text(280, 40, "30px", "Consolas", "black"); // text class
     background = new Background(0, 0, canvasWidth, canvasHeight, 0, 0, "city.jpeg"); // background class
-    obstacles.push(new Column(10, 200, 300, 120, 0, 0, "green"));
+    statBox = new StatBox(0, 0, canvasWidth, canvasHeight/15,"black",.3);
+    stats = new Text( 3, 40, "30px", "Consolas", "text","black"); // text class 
     gameArea.start();
 }
