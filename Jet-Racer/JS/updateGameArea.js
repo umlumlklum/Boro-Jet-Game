d6f8f0ef8fab1dcdb3f780be661744bde55a1fc5
NReedel <125296416+NReedel@users.noreply.github.com>
2024-02-22 18:36:36 -0500

    Update updateGameArea.js

--- Jet-Racer/JS/updateGameArea.js
+++ Jet-Racer/JS/updateGameArea.js
@@ -1,3 +1,42 @@
+this.startTime = new Date().getTime();
+
+function everyInterval(n){
+    if((gameArea.frameNumber / n) % 1 == 0){ 
+        return true; 
+    }
+
+    return false;
+}
+
+function fixLength(num, desiredLength) { 
+    // Convert the number to a string
+    let numStr = num.toString();
+  
+    // If the number is shorter than the desired length, pad with leading zeros
+    while (numStr.length < desiredLength) {
+      numStr = ' ' + numStr;
+    }
+  
+    // If the number is longer than the desired length, truncate
+    if (numStr.length > desiredLength) {
+      numStr = numStr.slice(0, desiredLength);
+    }
+  
+    return numStr;
+}
+
+function updateGameArea(){
+    let obstacleSpeed = -2;
+    let currentTime = new Date().getTime(); 
+    time = ((currentTime - startTime) / 1000).toFixed(2);  // Convert milliseconds to seconds
+    let x, height, gap, minHeight, maxHeight, minGap, maxGap;
+
+    for (let i = 0; i < obstacles.length; i++){
+        if (player.isOverlapping(obstacles[i])){
+            gameArea.stop();
+            return;
+        }
+    }
 
     gameArea.clear();
     gameArea.frameNumber += 1;
