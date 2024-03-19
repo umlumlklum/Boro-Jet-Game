324f870d932036187989b7a5e6643bc6ae921d55
NReedel <125296416+NReedel@users.noreply.github.com>
2024-03-18 21:10:20 -0400

    Update gameArea.js

--- Jet-Racer/JS/gameArea.js
+++ Jet-Racer/JS/gameArea.js
@@ -1,15 +1,15 @@
 class GameArea{
     constructor(){
         this.canvas = document.getElementById("canvas");
-    }
-
-    start(){
         this.canvas.width = canvasWidth;
         this.canvas.height = canvasHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNumber = 0;
-        this.interval = setInterval(updateGameArea, 1000 / frameRate);
+    }
+
+    start(){
+        this.interval = setInterval(updateGameArea, 15);
     }
 
     restart(){
@@ -17,7 +17,7 @@ class GameArea{
     }
 
     resume(){
-        this.interval = setInterval(updateGameArea, 1000 / frameRate);
+        this.start()
     }
 
     clear(){
