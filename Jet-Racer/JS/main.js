dd7758fa1be4f0430469e9e8600460a2c6cfe69a
NReedel <125296416+NReedel@users.noreply.github.com>
2024-04-02 20:20:04 -0400

    Update main.js

--- Jet-Racer/JS/main.js
+++ Jet-Racer/JS/main.js
@@ -1,4 +1,22 @@
+function loadLevel(){
+    var compBackgroundImage = window.getComputedStyle(background);
+    var backgroundImageUrl = compBackgroundImage.backgroundImage.replace(/^url\(['"]?([^'"]*)['"]?\)$/, '$1');
+    const hasZoneFiles = localStorage.getItem('zoneFiles') !== null;
+    if (hasZoneFiles){
+        const zoneFiles = JSON.parse(localStorage.getItem('zoneFiles'));
+        let currentZone = zoneFiles[0]+ "/" + zoneFiles[1][0];
+        let parts = backgroundImageUrl .split('/');
+        let startingZone = parts[parts.length-2]+ "/" + parts[parts.length-1];
+        if(currentZone !== startingZone){
+            backgroundImageUrl = backgroundImageUrl.replace(startingZone,currentZone);
+            console.log(backgroundImageUrl)
+            background.style.backgroundImage = 'url('+backgroundImageUrl+')';
+        }
+    }
+}
+
 function startGame(){
+    loadLevel();
     fetch('../JSON/level_config.json')
         .then(response => response.json())
         .then(data => levelConfig = data)
